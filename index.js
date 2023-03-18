9b59d50c70cacc15487730add780be100b435416
donovak <126442914+donovak@users.noreply.github.com>
2023-03-18 21:59:56 +1100

    Create initial game functions

---
+++ index.js
@@ -0,0 +1,31 @@
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock") 
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+getComputerChoice();
+
+
+
