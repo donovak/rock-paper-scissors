f806429899702cbf2bac637e570b7c519e5a2cb1
donovak <126442914+donovak@users.noreply.github.com>
2023-03-18 22:09:01 +1100

    Create playRound function
    Created the playRound function
    Included extra console.log tests to test that it was working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
+    console.log(choice);
     return choice;
 }
 
@@ -22,10 +23,24 @@ function checkWinner(playerSelection, computerSelection){
 }
 
 function playRound(playerSelection, computerSelection){
-
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie!"
+    }
+    else if(result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
 }
 
-getComputerChoice();
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+
 
 
 
